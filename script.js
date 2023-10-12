7638ab85f26ac8a1962e673389e8a39351442fbd
MyPc <brianw119@outlook.com>
2023-10-12 19:56:52 +0100

    Wrote test code to determine if player computer knows when a grid is empty

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ const game = {
     box6: null,
     box7: null,
     box8: null,
+    box9: null,
+
+    resultArr: [], 
         
     // the result for the game is stored in an array in an object
     userSelection: {
-        row1: [box1, box2, box3],
-        row2: [box4, box5, box6],
-        row3: [box7, box8, box9],
+        row1: [this.box1, this.box2, this.box3],
+        row2: [this.box4, this.box5, this.box6],
+        row3: [this.box7, this.box8, this.box9],
     },
    
 
@@ -38,28 +41,33 @@ const game = {
         for(const eachGrid of this.grid){
            eachGrid.addEventListener("click", event => {
 
-            if(manChooses){
+           /* if(manChooses){
                 eachGrid.innerHTML = "X";
             }else{
                 eachGrid.innerHTML = "O";
             };
+            */
 
         });
       };
     },
 
     computerChooses(){
-       for(const emptyBox of this.grid){
-          
+       this.resultArr = [];
+       for(let row in this.userSelection){
+          for(let index = 0; index < 3; index++){
+             if(this.userSelection[row][index] === null){
+                 this.resultArr.push(this.userSelection[row][index]);
+             };
+          };
        };
+       console.log(this.resultArr);
     },
 
-    
-    
     init(){
        window.addEventListener("load", () => this.typingIntro() );
        this.userChooses();
-    
+       this.computerChooses();
     },
 
 
